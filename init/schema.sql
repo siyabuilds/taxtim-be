ad91e0aec0a25a4e6cf6648f595a08fd53607af7
Athenkosi Magada <magadaathenkosi@gmail.com>
2026-02-15 20:31:51 +0200

    Remove unnecessary indexes from transactions and users tables in schema.sql; update CORS policy in index.php to allow all origins

--- init/schema.sql
+++ init/schema.sql
@@ -15,19 +15,9 @@ CREATE TABLE IF NOT EXISTS transactions (
     fiat_currency VARCHAR(5) DEFAULT 'ZAR'
 ) ENGINE=InnoDB;
 
-CREATE INDEX idx_transactions_date ON transactions (date);
-CREATE INDEX idx_transactions_type ON transactions (type);
-CREATE INDEX idx_transactions_buy_coin ON transactions (buy_coin);
-CREATE INDEX idx_transactions_sell_coin ON transactions (sell_coin);
-CREATE INDEX idx_transactions_buy_coin_date ON transactions (buy_coin, date DESC);
-CREATE INDEX idx_transactions_type_date ON transactions (type, date DESC);
-
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX idx_users_email ON users (email);
-CREATE INDEX idx_users_created_at ON users (created_at DESC);
+) ENGINE=InnoDB;
--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline) {
 });
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
